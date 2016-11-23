8171beb82151536913045ce1234fed795ae8f4dd
Mohamad Fachrur Ridwan <ridwan.fachrur@gmail.com>
2016-11-23 22:57:25 +0700

    Add stock services for intent and task to handle the stock data

---
+++ app/src/main/java/com/rfachrur/stockhawk/quotes/QuoteDatabase.java
@@ -0,0 +1,17 @@
+package com.rfachrur.stockhawk.quotes;
+
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
+/**
+ * Created by rfachrur on 11/23/16.
+ */
+
+@Database(version = QuoteDatabase.VERSION)
+public class QuoteDatabase {
+    private QuoteDatabase(){}
+
+    public static final int VERSION = 7;
+
+    @Table(QuoteColumns.class) public static final String QUOTES = "quotes";
+}
---
+++ app/src/main/java/com/rfachrur/stockhawk/stockservices/StockIntentService.java
@@ -0,0 +1,29 @@
+package com.rfachrur.stockhawk.stockservices;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.rfachrur.stockhawk.R;
+
+/**
+ * Created by rfachrur on 11/23/16.
+ */
+
+public class StockIntentService extends IntentService {
+
+    public StockIntentService() {
+        super(StockIntentService.class.getName());
+    }
+
+    public StockIntentService(String name) {
+        super(name);
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(StockIntentService.class.getSimpleName(), getResources().getString(R.string.stock_intent_service));
+        StockTaskService stockTaskService = new StockTaskService(this);
+    }
+}
