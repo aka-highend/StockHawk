9007083b5b823bddcb28973f64bea4e0be53993d
Mohamad Fachrur Ridwan <ridwan.fachrur@gmail.com>
2016-11-24 15:51:56 +0700

    Add : Add android design support dependency Fix : Initiate the MainActivity and DetailsActivity for displaying stock details

--- app/src/main/java/com/rfachrur/stockhawk/DetailsActivity.java
+++ app/src/main/java/com/rfachrur/stockhawk/DetailsActivity.java
@@ -1,8 +1,106 @@
 package com.rfachrur.stockhawk;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.rfachrur.stockhawk.fragments.FirstFragment;
+import com.rfachrur.stockhawk.fragments.SecondFragment;
+import com.rfachrur.stockhawk.fragments.ThirdFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by rfachrur on 11/23/16.
  */
 
-public class DetailsActivity {
+public class DetailsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    Bundle symbol;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_activity);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        String s = getIntent().getStringExtra(getResources().getString(R.string.symbol_label));
+
+        symbol = new Bundle();
+        symbol.putString(getString(R.string.symbol_bundle),s);
+
+        getSupportActionBar().setTitle(s.toUpperCase());
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FirstFragment(), getResources().getString(R.string.details));
+        adapter.addFragment(new SecondFragment(), getResources().getString(R.string.charts));
+        adapter.addFragment(new ThirdFragment(), getResources().getString(R.string.news));
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    FirstFragment f1 = new FirstFragment();
+                    f1.setArguments(symbol);
+                    return f1;
+                case 1:
+                    SecondFragment f2 = new SecondFragment();
+                    f2.setArguments(symbol);
+                    return f2;
+                case 2:
+                    ThirdFragment f3 = new ThirdFragment();
+                    f3.setArguments(symbol);
+                    return f3;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
